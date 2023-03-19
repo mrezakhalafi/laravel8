d09e366f8861a1345350415ddd3f9d66a84d2db6
M Reza Khalafi <64053794+mrezakhalafi@users.noreply.github.com>
2023-03-19 17:38:27 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<link rel="stylesheet" href="css/style.css">
+
+<body>
+    Ini Index
+    <p>Nama = <?= $name ?></p>
+    <p>Email = <?= $email ?></p>
+    <img src="img/WIN_20230212_18_34_13_Pro.jpg">
+</body>
+
+</html>
+<script src="js/script.js"></script>
