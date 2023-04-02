e83c93826695b05eb6e1da5cc1ad361e2699a2b9
M Reza Khalafi <64053794+mrezakhalafi@users.noreply.github.com>
2023-04-02 11:44:01 +0700

    Tutorial Until Video 20

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -82,7 +82,10 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        return view('dashboard/posts/edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -94,7 +97,27 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+        ];
+
+        if ($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+        }
+
+        $validateData = $request->validate($rules);
+
+
+        $validateData['user_id'] = auth()->user()->id;
+
+        $validateData['excerp'] = Str::limit(strip_tags($request->body), 100, '...');
+
+        Post::where('id', $post->id)
+            ->update($validateData);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been updated.');
     }
 
     /**
@@ -105,7 +128,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+
+        return redirect('/dashboard/posts')->with('success', 'Post has been deleted.');
     }
 
     public function checkSlug(Request $request)
--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -3,16 +3,17 @@
 @section('container')
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Create New Posts</h1>
+        <h1 class="h2">Edit New Posts</h1>
     </div>
 
     <div class="row">
         <div class="col-lg-8">
-            <form method="POST" action="/dashboard/posts" class="mb-5">
+            <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+                @method('put')
                 @csrf
                 <div class="mb-3">
                   <label for="title" class="form-label">Title</label>
-                  <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+                  <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $post->title) }}">
                     @error('title')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -21,7 +22,7 @@
                 </div>
                 <div class="mb-3">
                     <label for="slug" class="form-label">Slug</label>
-                    <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" readonly value="{{ old('slug') }}">
+                    <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" readonly value="{{ old('slug', $post->slug) }}">
                     @error('slug')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -35,7 +36,7 @@
                     @enderror
                     <select class="form-select" name="category_id" id="category_id">
                         @foreach($categories as $category)
-                            @if (old('category_id') == $category->id)
+                            @if (old('category_id', $post->category_id) == $category->id)
                                 <option value="{{ $category->id }}" selected>{{ $category->name }}</option>
                             @else
                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -44,15 +45,15 @@
                     </select>
                 </div>
                 <div class="mb-3">
-                    @error('category')
+                    <label for="body" class="form-label">Body</label>
+                    @error('body')
                         <p class="text-danger">{{ $message }}</p>
                     @enderror
-                    <label for="body" class="form-label">Body</label>
-                    <input id="body" type="hidden" name="body" value="{{ old('body') }}">
+                    <input id="body" type="hidden" name="body" value="{{ old('body', $post->body) }}">
                     <trix-editor input="body"></trix-editor>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Create Post</button>
+                <button type="submit" class="btn btn-primary">Edit Post</button>
               </form>
         </div>
     </div>
