d54e75dd364ad2838924cae065723b4dd411b379
M Reza Khalafi <64053794+mrezakhalafi@users.noreply.github.com>
2023-04-02 18:40:46 +0700

    Tutorial WPU Until Video 23

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 // use App\Models\Post;
@@ -95,3 +96,6 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show');
